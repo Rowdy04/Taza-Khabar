0a187bc23ece47b69e5bddc0b4eb177316be2f4a
Atharv Deshmukh <144980925+Rowdy04@users.noreply.github.com>
2025-01-03 18:01:33 +0530

    Update App.js

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -3,13 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import News from "./components/news";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Login from "./components/login";
+import Register from "./components/register";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <News />
-    </>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
